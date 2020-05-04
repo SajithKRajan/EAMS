35d26282f8fffbe4341147bd7dc48eca9d37a1d3
SajithKRajan <sajithkrajan07@gmail.com>
2020-05-04 21:41:25 +0530

    * Command name date type change, Added str2int in Utility.cpp

--- EAMSCommonLib/Entities/Command.h
+++ EAMSCommonLib/Entities/Command.h
@@ -1,5 +1,14 @@
 #pragma once
+#include <iostream>
+#include <string.h>
+#include <vector>
+
+using namespace std;
 class Command
 {
+public:
+	string function_handler_name;
+	char* command_name;
+	vector<string> inputs;
 };
 
--- EAMSCommonLib/Utilities/Utility.h
+++ EAMSCommonLib/Utilities/Utility.h
@@ -1,1 +1,30 @@
+#include "pch.h"
+#include <string>
+#include <iostream>
 
+class Utility
+{
+public:
+	static int parse_int(std::string value) {
+		int int_val = 0;
+		try
+		{
+			int int_val = std::stoi(value);
+		}
+		catch (std::invalid_argument const& e)
+		{
+			std::cout << "Bad input: Utility::parseInt thrown" << '\n';
+		}
+		catch (std::out_of_range const& e)
+		{
+			std::cout << "Integer overflow: Utility::parseInt thrown" << '\n';
+		}
+		return int_val;
+	}
+
+	static constexpr unsigned int str2int(const char* str, int h = 0)
+	{
+		return !str[h] ? 5381 : (str2int(str, h + 1) * 33) ^ str[h];
+	}
+
+};
