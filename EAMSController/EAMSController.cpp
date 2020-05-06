c7c0777dab697fb6107e3fce24b96123bc3307e4
SajithKRajan <sajithkrajan07@gmail.com>
2020-05-06 21:22:25 +0530

    removed unwanted.

--- EAMSController/EAMSController.cpp
+++ EAMSController/EAMSController.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include "Source/CommandParser.h"
+#include <stdlib.h>
+#include "Source/InputReader.h"
+#include "Common/EAMSFactory.h"
 
 using namespace std;
 
 int main()
 {
     std::cout << "Hello World!\n";
-    CommandParser* cp = cp->getInstance();
-    Command* cmd = cp->getCommand("VIEW_EMPLOYEE_DETAILS");
-    cout << endl;
+    //CommandParser* cp = cp->getInstance();
+    //Command* cmd = cp->getCommand("VIEW_EMPLOYEE_DETAILS");
+    //system("cls");
+    //InputReader* in = new InputReader();
+    //Command* cmd = in->getCommand("LOGIN");
+
+    IDataHandler* hanlder = EAMSFactory::Instance().getIDataHandler(HandlerTypes::EMPLOYEE);
+    Command* cmd = new Command();
+    cmd->command_name = (char*)"ADD_EMPLOYEE";
+    hanlder->execute(cmd);
+
+    cout << "fdgfdgfd" <<endl;
 }
 
