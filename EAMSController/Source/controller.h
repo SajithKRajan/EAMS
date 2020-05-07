4ca128706aa88344b5b4c754b9dd129572fd83e6
Jeslin K Raju <jeena576@gmail.com>
2020-05-07 10:39:02 +0530

    Controller class implementation

---
+++ EAMSController/Source/controller.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Common/EAMSFactory.h"
+#include"Entities/Command.h"
+#include "Source/InputReader.h"
+#include "Entities/ResultSet.h"
+
+class controller
+{
+public:
+	void start();
+};
+
