35bd104bc4c1d83b57f75257a617500998bbe834
Jeslin K Raju <jeena576@gmail.com>
2020-05-08 15:15:12 +0530

    DEmo

--- EAMSCommonLib/Utilities/Utility.h
+++ EAMSCommonLib/Utilities/Utility.h
@@ -36,4 +36,19 @@ public:
 		return data;
 	}
 
+	static std::string toCamelCase(std::string data, char delimitter) {
+		std::for_each(data.begin(), data.end(), [delimitter](char& ch) {
+			static int last = delimitter;
+			if (last == delimitter && ch != delimitter && ::isalpha(ch))
+				ch = ::toupper(ch);
+			else
+				ch = ::tolower(ch);
+			last = ch;
+			if (ch == delimitter)
+				ch = ' ';
+			});
+		data[0] = toupper(data[0]);
+		return data;
+	}
+
 };
